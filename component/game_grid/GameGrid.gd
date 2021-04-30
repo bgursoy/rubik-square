de46fad80e9c1ab85517a9c350759c18aa756e03
bgursoy <sbahargursoy@gmail.com>
2021-04-30 18:36:29 +0300

    Modift game scene

--- component/game_grid/GameGrid.gd
+++ component/game_grid/GameGrid.gd
@@ -1,37 +1,48 @@
 extends ReferenceRect
-
+const CELL_SPRITE_WIDTH = 150
 const SQUARE_SIZE = 6
-const SCALE = 0.333
+const SCALE = 0.30
 var RubikCell = preload("res://component/rubik_cell/RubikCell.gd")
 
 var square_width = 0
 var num_colors = 0
+var cell_scale = 1.0
+var rubic_cell_map = [] 
 
-func _init(viewport, square_width, num_colors):
+func _init(dim_x, square_width, num_colors):
 	print("Game board initiated")
 	self.square_width = square_width
 	self.num_colors = num_colors
 	
-	# Calculate scale according to the available space
-	# Prefer the smaller dimension
-	var dim_x = viewport;
-	print(dim_x)
+	# Calculate scale, fit to screen automatically
+	self.cell_scale = dim_x / (CELL_SPRITE_WIDTH * square_width)
+	print("Render game grid into " + str(dim_x) + " pixels.")
+	print("Scale computed for game grid: " + str(self.cell_scale) )
 
 func _ready():
 	print("Game board ready")
 	# Add squares to the scene
 	for row in range(self.square_width):
+		var rubic_cell_row = []
 		for col in range(self.square_width):
 			var colorCode = randi() % self.num_colors
 			var squareSprite = RubikCell.new(colorCode)
-			var positionX = SCALE * (75 + col*150)
-			var positionY = SCALE * (75 + row*150)
-			squareSprite.set_scale(Vector2(SCALE, SCALE))
+			var positionX = self.cell_scale * (75 + col*150)
+			var positionY = self.cell_scale * (75 + row*150)
+			squareSprite.set_scale(Vector2(self.cell_scale, self.cell_scale))
 			squareSprite.position = Vector2(positionX, positionY)
 			self.add_child(squareSprite)
+			rubic_cell_row.append(squareSprite)
+		rubic_cell_map.append(rubic_cell_row)
 
 # Touch drag event listener
-func _input(event):
-	if event is InputEventScreenDrag:
-		print(event.as_text())
+#func _input(event):
+#	if event is InputEventScreenDrag:
+#		print(event.as_text())
+		
 
+func detect_col_row():
+	pass
+
+func on_drag():
+	print("Drag")
--- scene/game/GameScene.gd
+++ scene/game/GameScene.gd
@@ -7,12 +7,22 @@ var _gameGrid
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var containerViewPort = $gameGridContainer.get_viewport()
-	self._gameGrid = GameGrid.new(containerViewPort, 6, 3)
-	$gameGridContainer.add_child(self._gameGrid)
+	var gameGridContainerRect = $VBoxContainer/gameGridContainer.get_rect()
+	# $VBoxContainer/gameGridContainer.rect_clip_content(true)
+	$VBoxContainer/gameGridContainer.set_custom_minimum_size(Vector2(gameGridContainerRect.size.x, gameGridContainerRect.size.x))
+	self._gameGrid = GameGrid.new(gameGridContainerRect.size.x, 8, 6)
+	$VBoxContainer/gameGridContainer.add_child(self._gameGrid)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_gameGridContainer_mouse_entered():
+	self._gameGrid.on_drag()
+
+
+func _on_gameGridContainer_resized():
+	print($VBoxContainer/gameGridContainer.get_rect())
